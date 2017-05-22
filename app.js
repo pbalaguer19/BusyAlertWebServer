ef86080498c41304b885a01d2fec76a512db9b78
Pau <pbalaguer19@gmail.com>
2017-05-22 18:57:50 +0200

    Port 3000

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var db = require('./testingdb.js');
 var index = require('./routes/index');
 
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3000;
 var userinfos = [];
 
 // view engine setup
