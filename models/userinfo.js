e7bd6536b86b7bcb5e73c9abb51922b17230db81
calv00 <javicalvosaxo@gmail.com>
2017-05-24 15:24:42 +0200

    Testing DELETE 10

--- models/userinfo.js
+++ models/userinfo.js
@@ -13,7 +13,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     extraData: {
-      type: DataTypes.JSON,
+      type: DataTypes.STRING,
       allowNull: false
     }
   });
